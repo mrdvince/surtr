867b9e96121c89e4393b0c6371acb6769fa4aba3
Vince <vincivenv@gmail.com>
2025-06-22 18:12:30 +0200

    add more realm terraform samples

--- iac/test_realm.tf
+++ iac/test_realm.tf
@@ -1,25 +1,48 @@
-resource "proxmox_realm" "test_oidc" {
-  realm        = "test-groups"
-  type         = "openid"
-  issuer_url   = "https://auth.example.com"
-  client_id    = "proxmox-test"
-  client_key   = "super-secret-key"
-  username_claim = "username"
-  autocreate     = true
-  default        = false
-  comment        = "Test realm with group options"
-  groups_overwrite = false
+resource "proxmox_realm" "minimal" {
+  realm      = "test-minimal"
+  type       = "openid"
+  issuer_url = "https://auth.example.com"
+  client_id  = "proxmox-test"
+  client_key = "super-secret-key"
+}
+
+resource "proxmox_realm" "full" {
+  realm             = "test-full"
+  type              = "openid"
+  issuer_url        = "https://auth.example.com"
+  client_id         = "proxmox-test"
+  client_key        = "super-secret-key"
+  username_claim    = "email"
+  autocreate        = true
+  default           = false
+  comment           = "Updated full realm with all options"
+  groups_overwrite  = true
+  groups_autocreate = false
+}
+
+resource "proxmox_realm" "authentik" {
+  realm             = "test-authentik"
+  type              = "openid"
+  issuer_url        = "https://${var.authentik_url}/application/o/proxmox-test/"
+  client_id         = var.client_id
+  client_key        = var.client_key
+  username_claim    = "username"
+  autocreate        = true
+  default           = false
+  comment           = "Realm with authentik test"
+  groups_overwrite  = true
   groups_autocreate = true
 }
 
-output "realm_name" {
-  value = proxmox_realm.test_oidc.realm
+
+output "minimal_realm" {
+  value = proxmox_realm.minimal.realm
 }
 
-output "realm_type" {
-  value = proxmox_realm.test_oidc.type
+output "full_realm" {
+  value = proxmox_realm.full.realm
 }
 
-output "issuer_url" {
-  value = proxmox_realm.test_oidc.issuer_url
+output "authentik_issuer_url" {
+  value = proxmox_realm.authentik.issuer_url
 }
